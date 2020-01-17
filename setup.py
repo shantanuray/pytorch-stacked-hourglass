9879fe6a6c516cb13e28604df8a35be2f09f8273
Shantanu Ray <chico.chego@gmail.com>
2020-01-17 14:38:21 -0800

    Updated for mouse keypoint detection and yogi

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
+"""Setup Configuration."""
 from setuptools import setup, find_packages
 
 
 setup(
     name='pytorch-stacked-hourglass',
-    version='1.0.0a6',
-    maintainer='Aiden Nibali',
-    url='https://github.com/anibali/pytorch-stacked-hourglass',
+    version='1.0.0a7',
+    description='Stacked Hourglass for Markerless tracking',
+    maintainer='Shantanu Ray',
+    url='https://github.com/shantanuray/pytorch-stacked-hourglass',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     include_package_data=True,
@@ -17,9 +19,11 @@ setup(
         'torch',
     ],
     classifiers=[
-        'Topic :: Scientific/Engineering :: Image Recognition',
+        'Topic :: Scientific/Engineering :: Keypoint Detection',
         'Intended Audience :: Science/Research',
         'Intended Audience :: Developers',
+        'Intended Audience :: Markerless tracking',
+        'Intended Audience :: Behavioral experiments',
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
